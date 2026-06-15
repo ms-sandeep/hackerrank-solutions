3942fe1bf9fe08421ba2ea0cebe5b4aae1b4cc6f
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-16 02:06:30 +0530

    ✅ Loops [python3] — approach1

---
+++ solutions/python3/easy/python-loops/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Problem     Loops
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-16, 02:06 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i**2)
