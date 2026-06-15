d919888499ab76a56968d0a1e72f9baf591dd07f
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-16 02:12:05 +0530

    ✅ Python: Division [python3] — approach1

---
+++ solutions/python3/easy/python-division/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Problem     Python: Division
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-16, 02:11 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
