764c93c62e8eee1b986b219deb617b82c01b93e6
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-22 15:40:20 +0530

    ✅ Select By ID [db2] — approach1

---
+++ solutions/db2/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,11 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-06-22, 03:39 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * FROM CITY WHERE ID=1661;
