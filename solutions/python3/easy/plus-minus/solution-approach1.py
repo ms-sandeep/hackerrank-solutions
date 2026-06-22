3f56e95e9000bfad756c8303827ed9f3f6637198
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-22 18:08:58 +0530

    ✅ Plus Minus [python3] — approach1

---
+++ solutions/python3/easy/plus-minus/solution-approach1.py
@@ -0,0 +1,45 @@
+# ──────────────────────────────────────────────────
+# Problem     Plus Minus
+# Difficulty  Easy
+# Subdomain   Warmup
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-22, 06:08 p.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    p=0
+    n=0
+    z=0
+    for i in arr:
+        if i>0:
+            p+=1
+        elif i<0:
+            n+=1
+        else:
+            z+=1
+    print(p/len(arr))
+    print(n/len(arr))
+    print(z/len(arr))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
