175780fcc47f624f465979a46f44a619a24421b6
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-22 15:33:30 +0530

    ✅ Select All [db2] — approach1

---
+++ solutions/db2/easy/select-all-sql/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select All
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-06-22, 03:33 p.m.
+-- ──────────────────────────────────────────────────
+
+
+
+select id, name, countrycode, district, population from city;
