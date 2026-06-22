80d5f106c10de51a0e85399fb558fef9bd4194f7
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-22 14:02:16 +0530

    ✅ Solve Me First [python3] — approach1

---
+++ solutions/python3/easy/solve-me-first/solution-approach1.py
@@ -0,0 +1,20 @@
+# ──────────────────────────────────────────────────
+# Problem     Solve Me First
+# Difficulty  Easy
+# Subdomain   Warmup
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-22, 02:02 p.m.
+# ──────────────────────────────────────────────────
+
+
+
+def solveMeFirst(a,b):
+	return a+b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
