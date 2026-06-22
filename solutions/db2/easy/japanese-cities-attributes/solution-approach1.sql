29ddd4c276b0179741113d38608703389856d444
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-22 15:42:23 +0530

    ✅ Japanese Cities' Attributes [db2] — approach1

---
+++ solutions/db2/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,11 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-06-22, 03:42 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * FROM CITY WHERE COUNTRYCODE='JPN';
