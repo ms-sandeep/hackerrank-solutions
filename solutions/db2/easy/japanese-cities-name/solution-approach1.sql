c5899c4611010ff3aa80a126e0b054343b8b80d4
Sai Sandeep <saisandeepmathuraju@gmail.com>
2026-06-22 15:46:52 +0530

    ✅ Japanese Cities' Names [db2] — approach1

---
+++ solutions/db2/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,11 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-06-22, 03:46 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN';
